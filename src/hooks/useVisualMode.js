7630ec50809c98056d08164d47b62d5b93cb60f0
AllenZZP <131452abc@gmail.com>
2022-01-21 13:50:13 -0800

    add replace parameter

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,15 +4,18 @@ export default function useVisualMode(initial) {
   const [mode, setmode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = newMode => {
+  const transition = (newMode, replace = false) => {
     setmode(newMode);
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, prev.length - 2), newMode]);
+    }
     setHistory((prev => [...prev, newMode]));
   };
 
   const back = () => {
     if (history.length > 1) {
-      setHistory((prev => [...prev.slice(0, prev.length - 1)]));
       setmode(history[history.length - 2]);
+      setHistory((prev => [...prev.slice(0, prev.length - 1)]));
     } else {
       return;
     }
