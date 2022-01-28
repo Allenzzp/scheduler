cdea084be840c39317cf79e32d168961f2569e50
AllenZZP <131452abc@gmail.com>
2022-01-28 00:10:32 -0800

    first push

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function() {
+  //just test oit!
   const [state, setState] = useState({
     day: "Monday",
     days: [],
