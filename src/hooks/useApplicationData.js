80fb6e785fb71e5c0377a4c3a57e47ea03c4f246
AllenZZP <131452abc@gmail.com>
2022-01-28 01:19:17 -0800

    implement reducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,10 +1,70 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import axios from "axios";
 
+function spotsRemaining(id) {
+  let res;
+  if (id / 5 <= 1) {
+    res = 0;
+  } else if (id / 5 <= 2) {
+    res = 1;
+  }  else if (id / 5 <= 3) {
+    res = 2;
+  }  else if (id / 5 <= 4) {
+    res = 3;
+  }  else {
+    res = 4;
+  }
+  return res;
+}
+
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case SET_DAY:
+      return { ...state, day: action.day };
+
+    case SET_APPLICATION_DATA:
+      const {days, appointments, interviewers} = action;
+      return { ...state, days, appointments, interviewers};
+
+    case SET_INTERVIEW: {
+      const {id, interview} = action;
+
+      const appointment = {
+        ...state.appointments[id],
+        interview
+      };
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      }
+      const index = spotsRemaining(id);
+      const changeDays = [...state.days];
+
+      if (!interview) {
+        changeDays[index].spots++;
+        return {...state, appointments, days: changeDays};
+      }
+      if (state.appointments[id].interview) {
+        return {...state, appointments};
+      }
+      changeDays[index].spots--;
+      return {...state, appointments, days: changeDays};
+    }
+
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
 export default function() {
-  //just test oit!
-  //this is bad!
-  const [state, setState] = useState({
+  
+  const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
@@ -12,47 +72,20 @@ export default function() {
   });
 
   function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: {...interview}
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-
     return axios.put(`/api/appointments/${id}`, {interview})
       .then((res) => {
-        if (!state.appointments[id].interview) {
-          let index = spotsRemaining(id);
-          const changeDays = [...state.days];
-          changeDays[index].spots--;
-          setState(state => ({ ...state, appointments, days: changeDays}));
-        } else {
-          setState(state => ({...state, appointments}));
-        }
+        dispatch({ type: SET_INTERVIEW, id, interview });
       });
   }
 
   function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
     return axios.delete(`/api/appointments/${id}`)
       .then((res) => {
-        let index = spotsRemaining(id);
-        const changeDays = [...state.days];
-        changeDays[index].spots++;
-        setState({...state, appointments, days: changeDays});
+        dispatch({type: SET_INTERVIEW, id, interview: null});
       });
   }
 
-  const setDay = day => setState({...state, day});
+  const setDay = day => dispatch({type: SET_DAY, day});
   
   useEffect(() => {
     Promise.all([
@@ -60,31 +93,14 @@ export default function() {
       axios.get("api/appointments"),
       axios.get("api/interviewers")
     ]).then((all) => {
-      setState(prev => {
-        return {...prev, 
-          days: all[0].data, 
-          appointments: all[1].data,
-          interviewers: all[2].data
-        };
+      dispatch({
+        type: SET_APPLICATION_DATA,
+        days: all[0].data, 
+        appointments: all[1].data,
+        interviewers: all[2].data
       });
     });
   }, []);
-
-  function spotsRemaining(id) {
-    let res;
-    if (id / 5 <= 1) {
-      res = 0;
-    } else if (id / 5 <= 2) {
-      res = 1;
-    }  else if (id / 5 <= 3) {
-      res = 2;
-    }  else if (id / 5 <= 4) {
-      res = 3;
-    }  else {
-      res = 4;
-    }
-    return res;
-  }
 
   return {
     state, 
